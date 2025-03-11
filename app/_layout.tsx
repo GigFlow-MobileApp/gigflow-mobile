7cb80c2b1a6502c3858b71e9d797da0e9caf8ff9
hightech-dev830 <goldendragon0830@gmail.com>
2025-03-11 08:44:37 -0400

    solve mounting error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,32 +1,6 @@
-import { Slot, useRouter, useSegments } from 'expo-router';
-import { useEffect, useState } from 'react';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-import SplashPage from "@/components/splash";
+import { Slot } from 'expo-router';
 import "../global.css";
 
 export default function RootLayout() {
-  const [isSplashVisible, setIsSplashVisible] = useState(true);
-  // const [isAuthChecked, setIsAuthChecked] = useState(false);
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const router = useRouter();
-  // const segments = useSegments(); // helps prevent re-routes on /auth
-
-  // Show splash for 3 seconds once
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsSplashVisible(false);
-      requestAnimationFrame(() => {
-        router.replace('/intro');
-      });
-    }, 3000); // 3 seconds
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  // Splash screen UI
-  if (isSplashVisible) {
-    return <SplashPage />;
-  }
-
   return <Slot />;
 }
---
+++ app/index.tsx
@@ -0,0 +1,5 @@
+import SplashPage from '@/components/splash';
+
+export default function Index() {
+  return <SplashPage />;
+}
