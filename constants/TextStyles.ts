05159444e54fa358f483beb94e4bbd7309800487
hightech-dev830 <goldendragon0830@gmail.com>
2025-03-11 11:19:39 -0400

    small changes

--- constants/TextStyles.ts
+++ constants/TextStyles.ts
@@ -1,34 +1,37 @@
 import { TextStyle } from "react-native";
+import { Platform } from "react-native";
+
+const ios = Platform.OS === 'ios';
 
 export const textStyles: Record<string, TextStyle> = {
   logo: {
     fontSize: 40,
-    fontWeight: "600",
+    fontWeight: ios ? "600" : "800",
     fontFamily: "Poppins",
   },
   title: {
     fontSize: 24,
-    fontWeight: "600",
+    fontWeight: ios ? "600" : "800",
     fontFamily: "Poppins",
   },
   subtitle: {
     fontSize: 24,
-    fontWeight: "600",
+    fontWeight: ios ? "600" : "800",
     fontFamily: "System",
   },
   section: {
     fontSize: 18,
-    fontWeight: "700",
+    fontWeight: ios ? "700" : "900",
     fontFamily: "Montserrat",
   },
   btnText: {
     fontSize: 18,
-    fontWeight: "400",
+    fontWeight: ios ? "400" : "600",
     fontFamily: "Montserrat",
   },
   description: {
     fontSize: 16,
-    fontWeight: "400",
+    fontWeight: ios ? "400" : "600",
     fontFamily: "Montserrat",
   },
   link: {
@@ -38,12 +41,12 @@ export const textStyles: Record<string, TextStyle> = {
   },
   default: {
     fontSize: 16,
-    fontWeight: "400",
+    fontWeight: ios ? "400" : "600",
     fontFamily: "Montserrat",
   },
   defaultSemiBold: {
     fontSize: 16,
     fontFamily: "Montserrat",
-    fontWeight: "700",
+    fontWeight: ios ? "700" : "900",
   },
 };
