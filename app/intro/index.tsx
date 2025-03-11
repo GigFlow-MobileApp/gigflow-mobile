61cf67ca5a932d11f8a9d1ceca86bb4c2d998dda
ziruiqiao <qiao.zir@northeastern.edu>
2025-03-11 02:16:57 -0400

    update auth page theme and font

--- app/intro/index.tsx
+++ app/intro/index.tsx
@@ -1,31 +1,38 @@
 // app/intro/index.tsx
 import { View, Text, Button, Image } from "react-native";
 import { router } from "expo-router";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { ThemedText } from "@/components/ThemedText";
+import { Colors } from "@/constants/Colors";
 
 export default function IntroScreen() {
+  const colorScheme = useColorScheme();
   return (
-    <View className="flex-1 justify-center bg-white px-4">
+    <View className="flex-1 justify-center bg-white px-4" style={{backgroundColor: Colors[colorScheme ?? "light"].background}}>
       <View className="items-center justify-center px-2">
         <View className={"items-center justify-start rounded-b-3xl"}>
-            <Image
-                source={require("@/assets/images/logo.png")}
-                className={"w-20 h-20 mb-2"}
-                resizeMode="contain"
-            />
-            <Text className={"font-bold pt-3 text-black text-5xl mt-8" }>
-                GIg-Flow
-            </Text>
+          <Image
+            source={require("@/assets/images/logo.png")}
+            className={"w-20 h-20 mb-2"}
+            resizeMode="contain"
+          />
+          <ThemedText className={"pt-3 mt-8"} type="logo" colorValue="primaryText">
+            GIg-Flow
+          </ThemedText>
         </View>
-        <Text className="text-center font-light text-gray-700 mb-6 pt-5 px-4" style={{fontSize: 18}}>
-          Manage your finances effortlessly{'\n'} with our intuitive app.
-        </Text>
+        <ThemedText className="text-center mb-6 pt-5 px-4" type="description" colorValue="secondaryText">
+          Manage your finances effortlessly{"\n"} with our intuitive app.
+        </ThemedText>
       </View>
-      <View className="absolute pb-10" style={{bottom: 0, alignSelf: "center", width: "100%"}}>
-        <View className="rounded-lg bg-cyan-500 overflow-hidden">
+      <View
+        className="absolute pb-10"
+        style={{ bottom: 0, alignSelf: "center", width: "100%" }}
+      >
+        <View className="rounded-lg overflow-hidden" style={{ backgroundColor: Colors[colorScheme ?? "light"].buttonBackground}}>
           <Button
             title="Get Started"
             onPress={() => router.replace("/auth")}
-            color="white"
+            color={Colors[colorScheme ?? "light"].text}
           />
         </View>
       </View>
