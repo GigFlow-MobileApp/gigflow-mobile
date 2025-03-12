5a3910543fd1885aec7358103bb201c09ad34299
ziruiqiao <qiao.zir@northeastern.edu>
2025-03-12 02:49:57 -0400

    add custom bottomTab menu instead of router/tabs

---
+++ components/BottomTabs.tsx
@@ -0,0 +1,57 @@
+import { View, TouchableOpacity} from "react-native";
+import { TabProps, TabItemsType } from "@/constants/customTypes";
+import { useColorScheme } from "@/components/ColorSchemeProvider";
+import { usePathname, useRouter } from "expo-router";
+import { Colors } from "@/constants/Colors";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import { ThemedText } from "@/components/ThemedText";
+
+export const BottomTabs = ({ items }: TabProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const { colorScheme } = useColorScheme();
+
+  return (
+    <View
+      className="h-[64px] flex-row justify-around items-center"
+      style={{ backgroundColor: Colors[colorScheme].background }}
+    >
+      {items.map((tab, idx) => (
+        <TouchableOpacity
+          key={idx}
+          onPress={() => router.push(tab.route as never)}
+        >
+          <View
+            className="bg-blue-500"
+            style={{
+              backgroundColor: Colors[colorScheme].background,
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <IconSymbol
+              name={pathname === tab.route ? tab.icon + ".fill" : tab.icon}
+              size={24}
+              color={
+                pathname === tab.route
+                  ? Colors[colorScheme].brandColor
+                  : Colors[colorScheme].menuItemText
+              }
+            />
+            <ThemedText
+              type="small"
+              style={{
+                color:
+                  pathname === tab.route
+                    ? Colors[colorScheme].brandColor
+                    : Colors[colorScheme].menuItemText,
+              }}
+            >
+              {tab.label}
+            </ThemedText>
+          </View>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+};
