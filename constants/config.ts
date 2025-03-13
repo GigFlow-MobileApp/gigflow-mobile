2090c07cb738babf3d8e5ca6a77eb91396accad8
hightech-dev830 <goldendragon0830@gmail.com>
2025-03-13 02:05:37 -0400

    update constants

--- constants/config.ts
+++ constants/config.ts
@@ -1,9 +1,9 @@
 const Config = {
-    debug: true,
+    debug: false,
     apiBaseUrl: "http://3.137.223.204:8000",
     version: "1.0.0",
-    username: "test5@gmail.com",
-    password: "12345678"
+    username: "usertest3@example.com",
+    password: "stringst"
   };
   
   export default Config;
