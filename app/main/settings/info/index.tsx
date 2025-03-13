cfcf4d9d18632146e0042094e3e66999576785de
hightech-dev830 <goldendragon0830@gmail.com>
2025-03-12 23:29:40 -0400

    implement setting page

---
+++ app/main/settings/info/index.tsx
@@ -0,0 +1,14 @@
+// app/(drawer)/setting.tsx
+import { View, Text } from 'react-native';
+
+export default function InfoScreen() {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+      <Text>Info Page</Text>
+    </View>
+  );
+}
