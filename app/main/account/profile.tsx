bde27adf2fc5a49a7b5bc75dae3a294415e20154
hightech-dev830 <goldendragon0830@gmail.com>
2025-03-24 14:30:49 -0400

    add animation to profile and activities page

--- app/main/account/profile.tsx
+++ app/main/account/profile.tsx
@@ -10,6 +10,7 @@ import { platformColor } from "@/constants/Colors";
 import { VictoryLine } from "victory-native";
 import { Icon } from "react-native-vector-icons/Icon";
 import { NumberProp } from "react-native-svg";
+import FadeInView, { SlideInView } from "@/components/FadeInView";
 
 const screenWidth = Dimensions.get("window").width;
 const screenHeight = Dimensions.get("window").height;
@@ -30,27 +31,28 @@ const capitalizeFirst = (str: string): string => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
-const InfoItem = ({ icon, text, value }: { icon: string; text: string; value: string }) => (
-  <View className="flex-row items-center my-2">
-    {/* <View> */}
-    <IconSymbol name={icon} size={24} color="#000" className="flex-col self-start pt-1" />
-    {/* </View> */}
-    <View className="flex-col items-start ml-1">
-      <ThemedText colorValue="primaryText" type="defautlSmall" className="p-1">
-        {text}
-      </ThemedText>
-      <ThemedText colorValue="textTertiary" type="defautlSmall" className="p-1">
-        {value}
-      </ThemedText>
+const InfoItem = ({ icon, text, value, index }: { icon: string; text: string; value: string; index: number }) => (
+  <FadeInView index={index}>
+    <View className="flex-row items-center my-2">
+      {/* <View> */}
+      <IconSymbol name={icon} size={24} color="#000" className="flex-col self-start pt-1" />
+      {/* </View> */}
+      <View className="flex-col items-start ml-1">
+        <ThemedText colorValue="primaryText" type="defautlSmall" className="p-1">
+          {text}
+        </ThemedText>
+        <ThemedText colorValue="textTertiary" type="defautlSmall" className="p-1">
+          {value}
+        </ThemedText>
+      </View>
     </View>
-  </View>
+  </FadeInView>
 );
 
 export default function ProfilePage() {
   const { name } = useLocalSearchParams();
   const router = useRouter();
   const { colors } = useThemeColors();
-  const [activities, setActivities] = useState<Activity[]>([]);
 
   // Function to convert rating to hearts/stars
   const ratingToHearts = (ratingValue: string): JSX.Element[] => {
@@ -150,10 +152,7 @@ export default function ProfilePage() {
       </View>
       {/* Top card section with fixed height */}
 
-      <ScrollView
-        className="h-auto"
-        showsVerticalScrollIndicator={false}
-      >
+      <ScrollView className="h-auto" showsVerticalScrollIndicator={false}>
         <View style={{ height: cardHeight }}>
           <Image
             source={require("@/assets/images/profile_bg.png")}
@@ -161,32 +160,35 @@ export default function ProfilePage() {
             className="h-1/2"
             resizeMode="cover"
           />
-          <View className="flex-row p-4">
-            <Image
-              source={logoSource}
-              style={{ width: 60, height: 60, borderRadius: 30, marginBottom: 12 }}
-              resizeMode="cover"
-            />
-            <View className="flex-col justify-between items-start pl-3">
-              <ThemedText colorValue="primaryText" style={{ fontWeight: 700 }}>
-                Driver Name
-              </ThemedText>
-              <ThemedText colorValue="textTertiary">driver</ThemedText>
-              <View className="flex-row items-center justify-between" style={{ width: cardWidth }}>
-                <View className="flex-row">
-                  <IconSymbol name="location" size={24} color={colors.brandColor} />
-                  <ThemedText colorValue="textTertiary">Onboarding_status</ThemedText>
+          <SlideInView direction='left'>
+            <View className="flex-row p-4">
+              <Image
+                source={logoSource}
+                style={{ width: 60, height: 60, borderRadius: 30, marginBottom: 12 }}
+                resizeMode="cover"
+              />
+              {/* Info Header */}
+              <View className="flex-col justify-between items-start pl-3">
+                <ThemedText colorValue="primaryText" style={{ fontWeight: 700 }}>
+                  Driver Name
+                </ThemedText>
+                <ThemedText colorValue="textTertiary">driver</ThemedText>
+                <View className="flex-row items-center justify-between" style={{ width: cardWidth }}>
+                  <View className="flex-row">
+                    <IconSymbol name="location" size={24} color={colors.brandColor} />
+                    <ThemedText colorValue="textTertiary">Onboarding_status</ThemedText>
+                  </View>
+                  <TouchableOpacity
+                    onPress={() => ""}
+                    className="mr-4 px-3 py-1 rounded-2xl"
+                    style={{ backgroundColor: colors.btnBackground }}
+                  >
+                    <ThemedText colorValue="btnText">online</ThemedText>
+                  </TouchableOpacity>
                 </View>
-                <TouchableOpacity
-                  onPress={() => ""}
-                  className="mr-4 px-3 py-1 rounded-2xl"
-                  style={{ backgroundColor: colors.btnBackground }}
-                >
-                  <ThemedText colorValue="btnText">online</ThemedText>
-                </TouchableOpacity>
               </View>
             </View>
-          </View>
+          </SlideInView>
         </View>
         {/* General Information Space */}
         <View style={{ flex: 1, backgroundColor: `${platformBgColor}36` }}>
@@ -194,67 +196,71 @@ export default function ProfilePage() {
             <ThemedText type="defaultSemiBold" colorValue="menuItemText" className="mb-2">
               General Information
             </ThemedText>
-            <InfoItem icon="email" text="Email" value={email} />
-            <InfoItem icon="app" text="Phone Number" value={phoneNumber} />
-            <View className="flex-row items-center my-2">
-              <IconSymbol name="star" size={24} color="#000" className="flex-col self-start pt-1" />
-              <View className="flex-col items-start ml-1">
-                <ThemedText colorValue="primaryText" type="defautlSmall" className="p-1">
-                  Rating
-                </ThemedText>
-                <View className="flex-row p-1">
-                  {ratingToHearts(rating)}
-                  <ThemedText colorValue="textTertiary" type="defautlSmall" className="ml-2">
-                    ({rating})
+            <InfoItem icon="email" text="Email" value={email} index={1} />
+            <InfoItem icon="app" text="Phone Number" value={phoneNumber} index={2} />
+            <FadeInView index={3}>
+              <View className="flex-row items-center my-2">
+                <IconSymbol name="star" size={24} color="#000" className="flex-col self-start pt-1" />
+                <View className="flex-col items-start ml-1">
+                  <ThemedText colorValue="primaryText" type="defautlSmall" className="p-1">
+                    Rating
                   </ThemedText>
+                  <View className="flex-row p-1">
+                    {ratingToHearts(rating)}
+                    <ThemedText colorValue="textTertiary" type="defautlSmall" className="ml-2">
+                      ({rating})
+                    </ThemedText>
+                  </View>
                 </View>
               </View>
-            </View>
-            <InfoItem icon="people" text="Partner Type" value={partnerType} />
-            <InfoItem icon="briefcase" text="Available Time" value={availableTime} />
-            <InfoItem icon="calendar" text="Joined" value={joined} />
+            </FadeInView>
+            <InfoItem icon="people" text="Partner Type" value={partnerType} index={4} />
+            <InfoItem icon="briefcase" text="Available Time" value={availableTime} index={5} />
+            <InfoItem icon="calendar" text="Joined" value={joined} index={6} />
           </View>
           {/* Graph Section */}
-          <View
-            style={{ backgroundColor: colors.backgroundCard }}
-            className="flex-row mx-4 mb-4 p-4 rounded-lg justify-between"
-          >
-            <View className="flex-col justify-between">
-              <ThemedText colorValue="primaryText" type="defautlSmall">
-                Total Trips
-              </ThemedText>
-              <ThemedText colorValue="brandColor" type="defautlSmall">
-                + 1250 Km
-              </ThemedText>
+          <FadeInView index={7}>
+            <View
+              style={{ backgroundColor: colors.backgroundCard }}
+              className="flex-row mx-4 mb-4 p-4 rounded-lg justify-between"
+            >
+              <View className="flex-col justify-between">
+                <ThemedText colorValue="primaryText" type="defautlSmall">
+                  Total Trips
+                </ThemedText>
+                <ThemedText colorValue="brandColor" type="defautlSmall">
+                  + 1250 Km
+                </ThemedText>
+              </View>
+              <View className="self-end">
+                <ThemedText
+                  colorValue={change > 0 ? "bullish" : "burish"}
+                  type="defautlSmall"
+                  style={{ fontWeight: 700 }}
+                >
+                  {change > 0 ? "+" : ""}
+                  {change}%
+                </ThemedText>
+              </View>
+              <View style={{ width: 80, height: 40 }}>
+                <VictoryLine
+                  padding={0}
+                  height={40}
+                  width={80}
+                  style={{
+                    data: { stroke: change > 0 ? colors.bullish : colors.burish, strokeWidth: 2 },
+                    parent: { border: "none" },
+                  }}
+                  data={pastData}
+                  domain={{
+                    y: [Math.min(...pastData.map((point) => point.y)), Math.max(...pastData.map((point) => point.y))],
+                  }}
+                  standalone={true}
+                  animate={false}
+                />
+              </View>
             </View>
-            <View className="self-end">
-              <ThemedText
-                colorValue={change > 0 ? "bullish" : "burish"}
-                type="defautlSmall"
-                style={{ fontWeight: 700 }}
-              >
-                {change > 0 ? "+" : ""}
-                {change}%
-              </ThemedText>
-            </View>
-            <View style={{ width: 80, height: 40 }}>
-              <VictoryLine
-                padding={0}
-                height={40}
-                width={80}
-                style={{
-                  data: { stroke: change > 0 ? colors.bullish : colors.burish, strokeWidth: 2 },
-                  parent: { border: "none" },
-                }}
-                data={pastData}
-                domain={{
-                  y: [Math.min(...pastData.map((point) => point.y)), Math.max(...pastData.map((point) => point.y))],
-                }}
-                standalone={true}
-                animate={false}
-              />
-            </View>
-          </View>
+          </FadeInView>
         </View>
       </ScrollView>
     </View>
